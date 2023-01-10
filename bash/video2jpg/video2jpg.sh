6b7d6e7c2b7fc6af9426a6a4db1d6ea5e550a8a7
urbanware-org <github@urbanware.org>
2023-01-10 20:36:11 +0100

    Rename method to avoid confusion

--- bash/video2jpg/video2jpg.sh
+++ bash/video2jpg/video2jpg.sh
@@ -1,8 +1,8 @@
-video2jpg() {
+convert_video2jpg() {
     input_video_file="$1"
     input_video_fps="$2"
     output_filename="$3"
-    
+
     if [ -z "$input_video_fps" ]; then
         input_video_fps=30
     fi
